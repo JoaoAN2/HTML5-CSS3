9fe49d8b4596459f57f39292c14abd550325df00
João do Nascimento <joao.an.221203@gmail.com>
2020-08-24 00:08:18 -0300

    All Course

---
+++ Google-Glass_HTML&CSS/HTML5 + CSS3/curso-html5-pacote01/projeto-glass-html5/_javascript/Script.js
@@ -0,0 +1,4 @@
+function change_picture(foto){
+    var icon = document.getElementById("icon")
+    icon.src = foto
+}
---
+++ Google-Glass_HTML&CSS/HTML5 + CSS3/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,5 @@
+function calc_total(){
+    var qntd = Number(document.getElementById("qntd").value)
+    tot = qntd * 1500
+    document.getElementById("value").value = tot
+}
